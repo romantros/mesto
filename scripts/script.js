8399b81f3a9ce98a8a651c41d3d64ab8a4448af6
romantros <rtrostin@gmail.com>
2021-01-24 23:53:18 +0300

    ПР4 версия5

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,26 @@
 let overlay = document.querySelector('.popup');
-let openPopupBtn = document.querySelector('.profile__editButton');
+let openPopupBtn = document.querySelector('.profile__edit-button');
 let closePopupBtn = overlay.querySelector('.popup__close');
-
 let formElement = overlay.querySelector('.popup__container');
-let nameInput = overlay.querySelector('.popup__name');
+let nameInput = overlay.querySelector('.popup__edit_name');
 let profileName = document.querySelector('.profile__name');
-let jobInput = overlay.querySelector('.popup__hobby');
+let jobInput = overlay.querySelector('.popup__edit_hobby');
 let profileHobby = document.querySelector('.profile__hobby');
 
 let togglePopup = function(evt) {
     evt.preventDefault();
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileHobby.textContent;
+    if(overlay.className === 'popup') {
+        nameInput.value = profileName.textContent;
+        jobInput.value = profileHobby.textContent;
+    }
     overlay.classList.toggle('popup_opened');
 }
+
 let closePopup = function(evt) {
     if (evt.target === evt.currentTarget) {
         togglePopup(evt);
     }
 }
-
-openPopupBtn.addEventListener('click', togglePopup);
-closePopupBtn.addEventListener('click', togglePopup);
-overlay.addEventListener('click', closePopup);
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
@@ -31,4 +29,7 @@ function formSubmitHandler (evt) {
     togglePopup(evt);
 }
 
+openPopupBtn.addEventListener('click', togglePopup);
+closePopupBtn.addEventListener('click', togglePopup);
+overlay.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
